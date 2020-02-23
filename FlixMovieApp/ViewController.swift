9a4a7c210a281f5e1c84a893957f819fb10ee8c1
Timothy Stewart <timothy.stewart18@yahoo.com>
2020-02-23 17:41:37 -0600

    Initial Commit

---
+++ FlixMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixMovieApp
+//
+//  Created by Timothy Stewart on 2/23/20.
+//  Copyright © 2020 Timothy Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
